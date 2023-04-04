838a4aaadbe5598c8d3f942d0fef2d0e19bc03b0
killian31 <killian.steunou@gmail.com>
2023-04-04 17:02:54 +0200

    feat(cmp): better cmp mapping and better copilot completion

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -31,6 +31,14 @@ return {
 		"zbirenbaum/copilot-cmp",
 		enabled = true,
 		dependencies = "zbirenbaum/copilot.lua",
-		config = true,
+		config = function()
+			require("copilot_cmp").setup({
+				formatters = {
+					label = require("copilot_cmp.format").format_label_text,
+					insert_text = require("copilot_cmp.format").remove_existing,
+					preview = require("copilot_cmp.format").deindent,
+				},
+			})
+		end,
 	},
 }
